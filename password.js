94d310f36d1f6d71a0b1b2676050a1939c3def96
blizzyblake278 <magicguitar19@gmail.com>
2021-03-31 10:50:03 -0500

    did not need to toggle disable attribute so it's not editable. Added password variable as to concat each rand char to itself in the FOR loop.

--- password.js
+++ password.js
@@ -1,7 +1,10 @@
 "use strict";
 $(document).ready(function() {
-    var getRandomNumber = function(max) {
-        var random;
+    let start, stop, char;
+    let password = "";
+
+    let getRandomNumber = function(max) {
+        let random;
         if (!isNaN(max)) {
             random = Math.random(); //value >= 0.0 and < 1.0
             random = Math.floor(random * max); //value is an integer between 0 and max - 1
@@ -12,9 +15,24 @@ $(document).ready(function() {
     
     $("#generate").click(function() {
         $("#password").val( "" ); // clear previous entry
-    
-        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@";
-        
+
+        let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@";
+        let userNumberEntered = parseInt($("#num").val())
+
+        if(isNaN(userNumberEntered)){
+            alert("Number of characters must be a number.");
+        }
+        else{
+            for (let i = 0; i < userNumberEntered; i++){
+                start = getRandomNumber(chars.length);
+                stop = start + 1;
+                char = chars.substring(start,stop);
+                password = password.concat(char)
+
+            }
+            $("#password").val(password);
+
+        }
     }); // end click()
     
     $("#clear").click(function() {
@@ -25,4 +43,7 @@ $(document).ready(function() {
     
     // set focus on initial load
     $("#num").focus();
+
+
+
 }); // end ready()
